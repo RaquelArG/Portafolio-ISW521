e18993779464fa84285332ce41cfc08e2a64cc63
RaquelArG <raquel.araya.granados@outlook.com>
2026-07-15 03:29:42 -0600

    feat:add config.template.js file

---
+++ Laboratorio-02/js/config.template.js
@@ -0,0 +1,15 @@
+export const CONFIG = {
+
+    BASE_URL: "https://worldcup26.ir",
+
+    EMAIL: "su correo",
+
+    PASSWORD: "su contraseña",
+
+    TOKEN: "su token"
+};
+
+export let CONFIG_TOKEN = {
+    TOKEN: "su token"
+};
+
